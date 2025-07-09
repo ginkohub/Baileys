5070d13d7ae32061a1fbf842cb8c4d220fc0805e
vini <viniwarrior4@gmail.com>
2025-07-09 16:01:20 -0300

    [feat]: add/edit/remove contacts (#1172)
    * add/edit/remove contacts
    
    * use proto type in contact action
    
    ---------
    
    Co-authored-by: canove <cassio@economicros.com.br>

--- src/Types/Chat.ts
+++ src/Types/Chat.ts
@@ -109,6 +109,7 @@ export type ChatModification =
 			lastMessages: LastMessageList
 	  }
 	| { delete: true; lastMessages: LastMessageList }
+	| { contact: proto.SyncActionValue.IContactAction | null }
 	// Label
 	| { addLabel: LabelActionBody }
 	// Label assosiation
